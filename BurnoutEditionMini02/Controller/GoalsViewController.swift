eeef8bc72e339189431fe513f327ad3e2d6566f7
mirellesine <mirellesine@gmail.com>
2023-09-22 14:30:59 -0300

    comitando

--- BurnoutEditionMini02/Controller/GoalsViewController.swift
+++ BurnoutEditionMini02/Controller/GoalsViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class GoalsViewController: UIViewController {
     
-    override func viewDidLoad() {
+    public weak var delegate: PopUpModalDelegate?
+    
+    private lazy var canvas: UIView = {
+    let view = UIView()
+    view.backgroundColor = .white
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.layer.cornerRadius = 10
+    view.clipsToBounds = true
+    return view
+    }()
+    
+   public override func viewDidLoad() {
+        
         super.viewDidLoad()
+        self.view.addSubview(canvas)
+        NSLayoutConstraint.activate([
+        self.canvas.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        self.canvas.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        self.canvas.widthAnchor.constraint(equalToConstant: self.view.bounds.width * 0.8),
+        self.canvas.heightAnchor.constraint(equalToConstant: self.view.bounds.height * 0.5),
+        ])
 
+       self.view.backgroundColor = UIColor.black.withAlphaComponent(0.2)
     }
+}
 
+public protocol PopUpModalDelegate: AnyObject {
+// add any functionalities that you want
+}
+
+@IBAction func didTapPresent(_ sender: UIButton) {
+let view =  GoalsViewController()
+view.delegate = self
+view.modalPresentationStyle = .overFullScreen
+view.modalTransitionStyle = .coverVertical
+self.present(view, animated: true)
 }
--- BurnoutEditionMini02/Controller/NewGoalModalViewController.swift
+++ BurnoutEditionMini02/Controller/NewGoalModalViewController.swift
@@ -7,23 +7,51 @@
 
 import UIKit
 
-class NewGoalModalViewController: UIViewController {
-
+class NewGoalModalViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var tasks = [String]()
+    var tableView: UITableView!
+    var taskTextField: UITextField!
+    var addButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Configurar a UITableView programaticamente
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TaskCell")
+        view.addSubview(tableView)
+        
+        // Configurar o campo de texto para adicionar tarefas
+        taskTextField = UITextField(frame: CGRect(x: 20, y: view.bounds.height - 100, width: view.bounds.width - 120, height: 40))
+        taskTextField.placeholder = "Digite uma nova tarefa"
+        view.addSubview(taskTextField)
+        
+        // Configurar o botão para adicionar tarefas
+        addButton = UIButton(type: .system)
+        addButton.frame = CGRect(x: view.bounds.width - 80, y: view.bounds.height - 100, width: 60, height: 40)
+        addButton.setTitle("Adicionar", for: .normal)
+        addButton.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+        view.addSubview(addButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    @objc func addTask() {
+        if let taskText = taskTextField.text, !taskText.isEmpty {
+            tasks.append(taskText)
+            taskTextField.text = ""
+            tableView.reloadData()
+        }
+    }
 }
+

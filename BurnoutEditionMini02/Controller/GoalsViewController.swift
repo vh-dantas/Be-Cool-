cc5972430fa45f02e2fe2b9098416a4a271c80a1
Thayna <thna.rdg@gmail.com>
2023-09-25 00:52:37 -0300

    Criando a lista de metas na home com dados da modal

--- BurnoutEditionMini02/Controller/GoalsViewController.swift
+++ BurnoutEditionMini02/Controller/GoalsViewController.swift
@@ -7,38 +7,99 @@
 
 import UIKit
 
-class GoalsViewController: UIViewController {
+// Implementa o protocolo NewGoalModalDelegate
+class GoalsViewController: UIViewController, NewGoalModalDelegate {
     
-    let goalsView = GoalsView()
+    // Criando uma stack view para mostrar os itens adicionados na modal
+    let goalsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical // Define a orientação da stack view como vertical
+        stackView.alignment = .leading // Alinha os itens à esquerda
+        stackView.distribution = .equalSpacing // Distribui os itens igualmente
+        stackView.spacing = 8 // Define o espaçamento
+        return stackView
+    }()
     
+    // Cria uma scroll view
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    // Função chamada quando a view é carregada
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Instância da View
-        view.addSubview(goalsView)
-        goalsView.setup()
+        view.backgroundColor = .white // Define a cor de fundo da view como branco
         
-        // Crie um botão
+        // Adiciona o scroll view à view e configura as constraints
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor), // Centraliza horizontalmente
+            scrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor), // Centraliza verticalmente
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9), // Define a largura como 90% da largura da view
+            scrollView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1) // Define a altura como 10% da altura da view
+        ])
+        
+        // Adiciona a goalsStackView ao scroll view e configura as constraints
+        scrollView.addSubview(goalsStackView)
+        goalsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            goalsStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            goalsStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            goalsStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            goalsStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            goalsStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
+        ])
+        
+        // MARK: Cria um botão para abrir a modal
         let button = UIButton(type: .system)
-        
-        // Defina o título do botão
         button.setImage(UIImage(systemName: "plus"), for: .normal)
-        
-        // Defina a posição e o tamanho do botão
         button.frame = CGRect(x: 100, y: 100, width: 200, height: 40)
-        
-        // Adiciona uma animação para ser executada quando o botão é tocado
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
-        // Adiciona o botão à view da tela
         view.addSubview(button)
     }
     
     @objc func buttonTapped() {
-        
         // Crie uma instância da new goal view controller modal que consegue ter navegação (NavigationController que embrulha um ViewController)
         let newGoalModalViewController = NewGoalModalViewController()
-        presentModal(viewController: newGoalModalViewController)
+        newGoalModalViewController.delegate = self // Define o delegate
+        presentModal(viewController: newGoalModalViewController) // Apresenta o modal
+    }
+    
+    // Função chamada quando uma nova meta é adicionada
+    func addedGoal(_ goal: String) {
+        // Cria uma stack view (item da lista)
+        let goalCheckItem = UIStackView()
+        goalCheckItem.axis = .horizontal // Define a orientação da stack view como horizontal
+        goalCheckItem.spacing = 8 // Define o espaçamento entre os itens
+        
+        // Cria o checkmark clicável
+        let checkmarkButton = UIButton(type: .system)
+        checkmarkButton.setImage(UIImage(systemName: "circle"), for: .normal)
+        checkmarkButton.addTarget(self, action: #selector(circleButtonTapped(_:)), for: .touchUpInside)
+        
+        // Cria o label que é o nome da meta
+        let goalName = UILabel()
+        goalName.text = goal
+        
+        // Adiciona os dois na stack view goalCheckItem
+        goalCheckItem.addArrangedSubview(checkmarkButton)
+        goalCheckItem.addArrangedSubview(goalName)
+        
+        self.goalsStackView.addArrangedSubview(goalCheckItem)
+    }
+    
+    @objc func circleButtonTapped(_ tappedButton: UIButton) {
+        if let buttonImage = tappedButton.currentImage {
+            if buttonImage == UIImage(systemName: "circle") {
+                tappedButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
+            } else {
+                tappedButton.setImage(UIImage(systemName: "circle"), for: .normal)
+            }
+        }
     }
 }
-        

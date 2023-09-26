3f69f1e8c99d54041ac902e99922f1e257e23338
mirellesine <mirellesine@gmail.com>
2023-09-26 15:54:28 -0300

    table view funfando das submetas

--- BurnoutEditionMini02/Controller/ModalViewControllers/NewSubgoalsModalViewController.swift
+++ BurnoutEditionMini02/Controller/ModalViewControllers/NewSubgoalsModalViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class NewSubgoalsModalViewController: UIViewController {
     
+    
+    
     // Cria um UILabel
     let label = UILabel()
     // Cria um UITextField
     let textField = UITextField()
     // Cria o botao de adicionar a meta
     let addButton = UIButton(type: .system)
-    
+    //Cria stack view
     let stackView = UIStackView()
     
     // Dependency Injection (injeção de dependências)
@@ -23,6 +25,11 @@ class NewSubgoalsModalViewController: UIViewController {
     
     //instancia da model subgoal
     var subgoals = [SubGoal]()
+    // Cria table view
+    
+    weak var delegate: NewSubGoalModalDelegate?
+    
+    var tableView: UITableView!
     
     
     init(goals: [Goal]) {
@@ -52,6 +59,23 @@ class NewSubgoalsModalViewController: UIViewController {
         addButton.setTitle("Adicionar", for: .normal)
         addButton.addTarget(self, action: #selector(addSubTask), for: .touchUpInside)
         
+        // Inicializa a table view
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+                
+        // Add constraints here
+//                tableView.translatesAutoresizingMaskIntoConstraints = false
+//                NSLayoutConstraint.activate([
+//                    tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//                    tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//                    tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//                    tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+//                ])
+            
+        
+        
         //Configura propriedades da StackView
         stackView.axis = .vertical //axis = eixo
         stackView.spacing = 16
@@ -70,24 +94,54 @@ class NewSubgoalsModalViewController: UIViewController {
         stackView.addArrangedSubview(label)
         stackView.addArrangedSubview(textField)
         stackView.addArrangedSubview(addButton)
+        stackView.addArrangedSubview(tableView)
         
         NSLayoutConstraint.activate([
             textField.heightAnchor.constraint(equalToConstant: 52),
-            addButton.heightAnchor.constraint(equalToConstant: 52)
+            addButton.heightAnchor.constraint(equalToConstant: 52),
+            tableView.heightAnchor.constraint(equalToConstant: 200)
         ])
     }
-    //função que adiciona subgoal
+    ///função que adiciona subgoal
     @objc func addSubTask() {
         if let subGoalText = textField.text, !subGoalText.isEmpty {
-            
+            delegate?.addedSubGoal(subGoalText)
             let subgoal = SubGoal(id: UUID(), title: subGoalText, time: Date(), type: .work)
             subgoals.append(subgoal)
             textField.text = ""
             
+            tableView.reloadData()
+            
             // cria a navegacao de push entre as modais
-            let NewWellnessSubgoalsModalViewController = NewWellnessSubgoalsModalViewController()
-            navigationController?.pushViewController(NewWellnessSubgoalsModalViewController, animated: true)
+//            let NewWellnessSubgoalsModalViewController = NewWellnessSubgoalsModalViewController()
+//            navigationController?.pushViewController(NewWellnessSubgoalsModalViewController, animated: true)
+            
+            
         }
     }
 }
+
+// Extend o view controller pra entrar em conformidade com a UITableViewDelegate e UITableViewDataSource
+extension NewSubgoalsModalViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subgoals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let subgoal = subgoals[indexPath.row]
+        
+        cell.textLabel?.text = subgoal.title
+        
+        return cell
+    }
+
+}
+
+protocol NewSubGoalModalDelegate: AnyObject {
+    func addedSubGoal(_ subgoal: String)
+}
+
 

864302825a65ab4f843313a54c7e396dee1e444c
João Victor <jvbernardes420@gmail.com>
2023-10-03 15:56:39 -0300

    Dynamic Types mais ou menos na Reflection Detail

--- BurnoutEditionMini02/Controller/Reflection/ReflectionDetailViewController.swift
+++ BurnoutEditionMini02/Controller/Reflection/ReflectionDetailViewController.swift
@@ -8,43 +8,128 @@ import UIKit
 
 class ReflectionDetailViewController: UIViewController {
     
+    // View que ira armazenar todas os componentes
+    let contentView = UIView()
+    // Scroll View
+    let scrollView = UIScrollView()
     // Label da data
     let dateLabel = UILabel()
-    
-    // Label para o body da reflection
+    // Labl para o body da reflection
     let bodyLabel = UITextView()
-    
     // Criando a StackView para por as Tags
     let tagStackView = UIStackView()
+    var tagCollectionView: UICollectionView! = nil
     // Criando as tags
-    let tagMood = RectangleLabelView(frame: CGRect(x: 0, y: 0, width: 100, height: 30), text: "vaaaaaaai")
-    let tagGoal = RectangleLabelView(frame: CGRect(x: 0, y: 0, width: 100, height: 30), text: "Meta muito foda aaaa")
-    
-    
+    let tagMood = RectangleLabelView(frame: CGRect(x: 0, y: 0, width: 100, height: 300))
+    let tagGoal = RectangleLabelView(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+    // Array que armazena os nomes das tags
+    let tags = ["Paralelepipedo Paralelepipedo", "Tag aasdawd2"]
+    // MARK: Função ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
         //Setando o titulo
         navigationItem.title = "Refletion 1"
         navigationController?.navigationBar.prefersLargeTitles = true
+        // Configurando a ScrollView
+        setUpScrollView()
+        //Configurando a ContentView
+        setUpContentView()
         // Configurando a Label de tempo
         configDateLabel()
         // Configurando as Tags
-        view.addSubview(tagStackView)
-        tagStackView.addArrangedSubview(tagGoal)
-        tagStackView.addArrangedSubview(tagMood)
-        configureTags()
-        
+       // view.addSubview(tagStackView)
+       // tagStackView.addArrangedSubview(tagGoal)
+       // tagStackView.addArrangedSubview(tagMood)
+       // configureTags()
+        // Configurando as Tags com CollectionVirw
+        setUpCollectionView()
         // Configurando a BodyLabel
         view.addSubview(bodyLabel)
         configureBodyLabel()
         
     }
     
- 
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            self.tagCollectionView.reloadData()
+        }
+      
+    }
+    // MARK: ScrollView
+    func setUpScrollView(){
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
     
-    // Função para configurar a data da reflection
+    
+    // MARK: ContentView
+    func setUpContentView() {
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
+    }
+    
+    // MARK: CollectionView
+    func setUpCollectionView(){
+ 
+        let flowLayout = UICollectionViewFlowLayout()
+           flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+           flowLayout.scrollDirection = .horizontal
+           flowLayout.minimumInteritemSpacing = 10
+           flowLayout.minimumLineSpacing = 6
+        
+       
+        tagCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        tagCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        tagCollectionView.dataSource = self
+        tagCollectionView.backgroundColor = .white
+        tagCollectionView.register(RectangleLabelView.self, forCellWithReuseIdentifier: RectangleLabelView.reuseIdentifier)
+        tagCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(tagCollectionView)
+        
+        // Pegar o tamanho da fonte para conseguir calcular o tamanho necessario para as tags
+        let multiplierHeight: CGFloat = {
+            var multiplier = CGFloat()
+            if dateLabel.font.pointSize >= 28 {
+                multiplier = 0.2
+            } else if dateLabel.font.pointSize <= 12 {
+                multiplier = 0.07
+            } else if dateLabel.font.pointSize >= 13 && dateLabel.font.pointSize <= 18 {
+                multiplier = 0.8
+            } else {
+                multiplier = 0.15
+            }
+            return multiplier
+        }()
+        
+        NSLayoutConstraint.activate([
+            tagCollectionView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 10),
+            tagCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+           // tagCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            tagCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplierHeight),
+            tagCollectionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.99)
+        ])
+        
+        
+        }
+    
+    
+    // Função para configurar a data da reflection MARK: DateLabel
     func configDateLabel() {
         dateLabel.translatesAutoresizingMaskIntoConstraints = false // Important for Auto Layout
         
@@ -57,16 +142,6 @@ class ReflectionDetailViewController: UIViewController {
         dateLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
         dateLabel.numberOfLines = 0
         dateLabel.lineBreakMode = .byWordWrapping
-        // Desembrulhando a nav bar para conseguir acessar suas constraitns e adicionar a label
-//        if let navigationBar = self.navigationController?.navigationBar {
-//            navigationBar.addSubview(dateLabel)
-//            // A constraint top é ancorada com o titulo - a leading possui constant para alinhar com o titulo
-//            NSLayoutConstraint.activate([
-//                dateLabel.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
-//                dateLabel.leadingAnchor.constraint(equalTo: navigationBar.leadingAnchor, constant: 17)
-//            ])
-//
-//        }
         // Adicionando a dateLabel na view - seus anchor sao de acordo com a safe area e as constants são para alinhar melhor com o titulo
         view.addSubview(dateLabel)
         NSLayoutConstraint.activate([
@@ -77,7 +152,7 @@ class ReflectionDetailViewController: UIViewController {
         ])
     }
     
-    // Função para configurar as tags - elas estao dentro de uma UIStackView entao configuro aqui tambem
+    // Função para configurar as tags - elas estao dentro de uma UIStackView entao configuro aqui tambem MARK: Tags
     func configureTags(){
 
         tagGoal.translatesAutoresizingMaskIntoConstraints = false
@@ -88,10 +163,6 @@ class ReflectionDetailViewController: UIViewController {
         tagStackView.spacing = 16
         tagStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        let tags = ["METAAAAA FODAAAA", "Tag 2", "Raiva e morte e tristeza", "Cabuloso"]
-        
-        
-       
         NSLayoutConstraint.activate([
             tagStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
             tagStackView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 21)
@@ -99,7 +170,7 @@ class ReflectionDetailViewController: UIViewController {
         ])
     }
     
-    // Func=ção para configurar o texto vody da reflection - usado UITextView para fazer o ajuste automatico na tela
+    // Função para configurar o texto vody da reflection - usado UITextView para fazer o ajuste automatico na tela MARK: BodyLabel
     func configureBodyLabel(){
         bodyLabel.translatesAutoresizingMaskIntoConstraints = false
    //     bodyLabel.textAlignment =
@@ -109,7 +180,7 @@ class ReflectionDetailViewController: UIViewController {
         bodyLabel.isEditable = false
         bodyLabel.isScrollEnabled = false
         NSLayoutConstraint.activate([
-            bodyLabel.topAnchor.constraint(equalTo: tagStackView.bottomAnchor, constant: 35),
+            bodyLabel.topAnchor.constraint(equalTo: tagCollectionView.bottomAnchor), // constant: 35
             bodyLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 12),
             bodyLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12)
             
@@ -118,13 +189,33 @@ class ReflectionDetailViewController: UIViewController {
     }
 }
 
+// MARK: DataSource
+extension ReflectionDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tags.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RectangleLabelView.reuseIdentifier, for: indexPath) as! RectangleLabelView
+        cell.configure(with: tags[indexPath.item])
+        return cell
+    }
+    
+    // Implementar o método sizeForItemAt para calcular o tamanho das células com base no conteúdo
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+         let tag = tags[indexPath.item]
+         let labelSize = (tag as NSString).size(withAttributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body)])
+         return CGSize(width: labelSize.width + 40, height: labelSize.height + 20)
+     }
+    
+}
 
-
-class RectangleLabelView: UIView {
+class RectangleLabelView: UICollectionViewCell {
+    static let reuseIdentifier = "cell-reuse-identifier"
+    
     private let label: UILabel = {
         let label = UILabel()
-        label.textAlignment = .center
+        label.textAlignment = .left
         label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
@@ -133,12 +224,11 @@ class RectangleLabelView: UIView {
         return label
     }()
     
-     init(frame: CGRect, text: String) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .gray // Cor de fundo do retângulo
         addSubview(label)
          // Colocando o texto na label
-         label.text = text
          
          NotificationCenter.default.addObserver(self, selector: #selector(handleContentSizeCategoryChange), name: UIContentSizeCategory.didChangeNotification, object: nil)
          
@@ -162,6 +252,9 @@ class RectangleLabelView: UIView {
         NotificationCenter.default.removeObserver(self)
     }
     
+    func configure(with text: String) {
+          label.text = text
+      }
     
     // Função para atualizar o corner da tag quando ela for trocada no sistema - vai ser usada na notification
     @objc private func handleContentSizeCategoryChange(){
@@ -172,7 +265,7 @@ class RectangleLabelView: UIView {
     // Rounded Rectangle de acordo com o tamanho da fonte no sistema
      func updateCornerRadius(){
         let fontSize = label.font.pointSize
-        var maxCornerRadius: CGFloat = 20 // Valor máximo para cornerRadius
+        var maxCornerRadius: CGFloat = 17 // Valor máximo para cornerRadius
         let minCornerRadius: CGFloat = 5 // Valor mínimo para cornerRadius
         
         // Verificando caso a fonte seja muito grande

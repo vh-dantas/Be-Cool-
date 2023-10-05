f498753189c01ffb13908358f9dcb6ea067b8843
Thayna <thna.rdg@gmail.com>
2023-10-05 13:55:54 -0300

    adicionando scroll nos creditos e criando a tela de preferencias

--- BurnoutEditionMini02/Controller/Settings/AcknowledgementsViewController.swift
+++ BurnoutEditionMini02/Controller/Settings/AcknowledgementsViewController.swift
@@ -12,7 +12,7 @@ class AcknowledgementsViewController: UIViewController, UITableViewDelegate, UIT
     let tableView1 = UITableView()
     let tableView2 = UITableView()
     let tableView3 = UITableView()
-    let devs = ["Amanda", "João", "Mirelle", "Thayná", "Victor Hugo"]
+    let devs = ["Amanda Parrini", "João Victor Bernardes", "Mirelle Sine", "Thayná Rodrigues", "Victor Hugo Dantas"]
     let experts = ["Edlane", "Nathali"]
     let mentors = ["Jair Barbosa", "Julio Santos", "Felipe Carvalho"]
     
@@ -20,50 +20,101 @@ class AcknowledgementsViewController: UIViewController, UITableViewDelegate, UIT
         super.viewDidLoad()
         self.view.backgroundColor = .systemGray6
         
+        // Create a UIScrollView
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scrollView)
+        
+        // Create a container UIView inside the scroll view
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(containerView)
+        
         let titleLabel = UILabel()
         titleLabel.text = "about".localized
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
-        self.view.addSubview(titleLabel)
+        containerView.addSubview(titleLabel)
+        
+        let label1 = UILabel()
+        label1.text = "team".localized
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(label1)
+        
+        let label2 = UILabel()
+        label2.text = "experts".localized
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(label2)
+        
+        let label3 = UILabel()
+        label3.text = "mentors".localized
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(label3)
         
         let cellHeight: CGFloat = 44
         let totalRowsHeight1 = cellHeight * CGFloat(devs.count)
         let totalRowsHeight2 = cellHeight * CGFloat(experts.count)
         let totalRowsHeight3 = cellHeight * CGFloat(mentors.count)
         
-        setupTableView(tableView: tableView1, items: devs, yPosition: 60, totalRowsHeight: totalRowsHeight1)
-        setupTableView(tableView: tableView2, items: experts, yPosition: 60 + totalRowsHeight1 + 20, totalRowsHeight: totalRowsHeight2)
-        setupTableView(tableView: tableView3, items: mentors, yPosition: 60 + totalRowsHeight2 + 20, totalRowsHeight: totalRowsHeight3)
+        setupTableView(tableView: tableView1, items: devs, yPosition: 60, totalRowsHeight: totalRowsHeight1, containerView: containerView)
+        setupTableView(tableView: tableView2, items: experts, yPosition: 60 + totalRowsHeight1 + 20, totalRowsHeight: totalRowsHeight2, containerView: containerView)
+        setupTableView(tableView: tableView3, items: mentors, yPosition: 60 + totalRowsHeight2 + 20, totalRowsHeight: totalRowsHeight3, containerView: containerView)
         
         NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor),
             
-            tableView1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
-            tableView1.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
-            tableView1.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            // Set constraints for the labels
+            label1.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            label1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
+            
+            tableView1.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 30),
+            tableView1.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            tableView1.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
             tableView1.heightAnchor.constraint(equalToConstant: totalRowsHeight1),
             
-            tableView2.topAnchor.constraint(equalTo: tableView1.bottomAnchor, constant: 20),
-            tableView2.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
-            tableView2.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            label2.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            label2.topAnchor.constraint(equalTo: tableView1.bottomAnchor, constant: 20),
+
+            tableView2.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 20),
+            tableView2.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            tableView2.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
             tableView2.heightAnchor.constraint(equalToConstant: totalRowsHeight2),
             
-            tableView3.topAnchor.constraint(equalTo: tableView2.bottomAnchor, constant: 20),
-            tableView3.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
-            tableView3.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            label3.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            label3.topAnchor.constraint(equalTo: tableView2.bottomAnchor, constant: 20),
+
+            tableView3.topAnchor.constraint(equalTo: label3.bottomAnchor, constant: 20),
+            tableView3.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            tableView3.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
             tableView3.heightAnchor.constraint(equalToConstant: totalRowsHeight3),
+            
+            // Set constraints for scrollView and containerView
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: tableView3.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            // Set scrollView content size
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20) // Adjust this constant as needed
         ])
     }
     
-    func setupTableView(tableView: UITableView, items: [String], yPosition: CGFloat, totalRowsHeight: CGFloat) {
-        tableView.frame = CGRect(x: 10, y: yPosition, width: self.view.bounds.width - 20, height: totalRowsHeight)
+    func setupTableView(tableView: UITableView, items: [String], yPosition: CGFloat, totalRowsHeight: CGFloat, containerView: UIView) {
+        tableView.frame = CGRect(x: 10, y: yPosition, width: containerView.bounds.width - 20, height: totalRowsHeight)
         tableView.layer.cornerRadius = 10
         tableView.backgroundColor = .white
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.isScrollEnabled = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(tableView)
+        containerView.addSubview(tableView)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
